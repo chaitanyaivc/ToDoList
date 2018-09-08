ab01295ad9fd45966772748f24b8a7ff4ba8b8ca
Vishnu <chaitanyaivc@gmail.com>
2018-09-08 23:06:41 +0530

    .

--- ToDoList/src/app/to-do/to-do.component.ts
+++ ToDoList/src/app/to-do/to-do.component.ts
@@ -45,6 +45,7 @@ export class ToDoComponent implements OnInit {
     for(let i = 0; i < this.items.length; i++) {
       if(this.items[i].isDone) {
         this.items.splice(i, 1);
+        i--;
       }
     }
   }
